d449cae71fceb14ad2b5e382055762542229b060
Gabriel Phelipe <35767049+gphelipe1@users.noreply.github.com>
2018-04-19 15:12:44 -0300

    Update ordena.py

--- ordena.py
+++ ordena.py
@@ -14,6 +14,8 @@ def insertionSortfreq(ar1,ar2):
         ar2[j+1] = key2
         ar1[j+1] = key
 
+#~..................................
+
 def insertionSortAlfa(ar1):
 
     for i in range(1, len(ar1)):
@@ -45,7 +47,6 @@ def partition(arr,low,high):
  
     arr[i+1],arr[high] = arr[high],arr[i+1]
     return ( i+1 )
- 
 
 # arr[] --> Array 
 # low  --> index inicial
@@ -61,7 +62,9 @@ def quickSortAlfa(arr,low,high):
         # se encontram antes e depois da particao
         quickSortAlfa(arr, low, pi-1)
         quickSortAlfa(arr, pi+1, high)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#...................................
+
 def partition2(arr,low,high,arr2):
     i = ( low-1 )         # index do menor elemento
     pivot = arr[high]     # pivo
@@ -129,7 +132,7 @@ def shellSortAlfa(arr):
             # put temp (the original a[i]) in its correct location
             arr[j] = temp
         gap //= 2
-
+#~..................................
 def shellSortFreq(arr,arr2):
  
     # Start with a big gap, then reduce the gap
@@ -199,6 +202,45 @@ def heapSortAlfa(arr):
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]   # swap
         heapify(arr, i, 0)
+
+#~..................................
+
+def heapifyF(arr, n, i,arr2):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+ 
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i] > arr[l]:
+        largest = l
+ 
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] > arr[r]:
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+        arr2[i],arr2[largest] = arr2[largest],arr2[i]
+ 
+        # Heapify the root.
+        heapifyF(arr, n, largest,arr2)
+ 
+# The main function to sort an array of given size
+def heapSortFreq(arr,arr2):
+    n = len(arr)
+ 
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        heapifyF(arr, n, i,arr2)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        arr2[i], arr2[0] = arr2[0], arr2[i] 
+        heapifyF(arr, i, 0,arr2)
 #===============================================
 
 #==============BINARY_INSERTIONSORT=============
@@ -213,7 +255,7 @@ def binary_search(arr, val, start, end):
         else:
             return start+1
  
-    # this occurs if we are moving beyond left's boundary
+    # this occurs if we are moving bseyond left's boundary
     # meaning the left boundary is the least position to
     # find a number greater than val
     if start > end:
@@ -232,4 +274,39 @@ def binary_insert_sortAlfa(arr):
         val = arr[i]
         j = binary_search(arr, val, 0, i-1)
         arr[:] = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+
+#.....................................
+
+def binary_searchFreq(arr, val, start, end,arr2):
+    # we need to distinugish whether we should insert
+    # before or after the left boundary.
+    # imagine [0] is the last step of the binary search
+    # and we need to decide where to insert -1
+    if start == end:
+        if arr[start] < val:
+            return start
+        else:
+            return start+1
+ 
+    # this occurs if we are moving beyond left's boundary
+    # meaning the left boundary is the least position to
+    # find a number greater than val
+    if start > end:
+        return start
+ 
+    mid = (start+end)//2
+    if arr[mid] < val:
+        return binary_searchFreq(arr, val, mid+1, end,arr2)
+    elif arr[mid] > val:
+        return binary_searchFreq(arr, val, start, mid-1,arr2)
+    else:
+        return mid
+ 
+def binary_insert_sortFreq(arr,arr2):
+    for i in range(1, len(arr)):
+        val = arr[i]
+        val2=arr2[i]
+        j = binary_searchFreq(arr, val, 0, i-1,arr2)
+        arr[:] = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+        arr2[:] = arr2[:j] + [val2] + arr2[j:i] + arr2[i+1:]
 #==============================================
